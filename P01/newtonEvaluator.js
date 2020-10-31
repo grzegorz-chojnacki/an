751c6d1d700e4e738866d959189b763050174889
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-10-31 12:45:15 +0100

    Added some theory

--- P01/newtonEvaluator.js
+++ P01/newtonEvaluator.js
@@ -15,6 +15,32 @@ class NewtonEvaluator {
     this.points = points
   }
 
+  getPolynomials() {
+    /**   x :  2, -1, 3, 1
+     * f(x):  4, 1, 17, 1
+     *
+     *
+     * P(x) = b0 * P0
+     *      + b1 * P1
+     *      + b2 * P2
+     *      + b3 * P3
+     *
+     * P0 = f(x0) = 4
+     *    => [4]
+     *
+     * P1 = (x - x0) = (x - 2)
+     *    => [-2, 1]
+     *
+     * P2 = (x - x0)(x - x1) = (x - 2)(x + 1) => (x^2 - x - 2)
+     *    => [-2, -1, 1]
+     *
+     * P3 = (x - x0)(x - x1)(x - x2) = (x - 2)(x + 1)(x - 3)
+     *    = (x^3 - 4x^2 + x + 6)
+     *    => [6, 1, -4, 1]
+     *
+     */
+  }
+
   getBs() {
     // ToDo: Memoizing
     const xMult = (points, init = 1) => (x) =>
