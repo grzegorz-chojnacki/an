2a0ee1680e6b6cfabe4834002a8192c69a1fac19
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2020-11-03 20:35:56 +0100

    Added addition tests

--- P01/polynomial.spec.js
+++ P01/polynomial.spec.js
@@ -18,4 +18,17 @@ const PolynomialTests = (() => {
   expect(formulaOf([1,0,1])).toBe('x^2 + 1')
   expect(formulaOf([2,-1,2,-2])).toBe('-2x^3 + 2x^2 - x + 2')
   expect(formulaOf([0,0,0,0,0,4])).toBe('4x^5')
+
+  const additionOf = (termsA, termsB) =>
+    (new Polynomial(termsA)).add(new Polynomial(termsB)).terms
+
+  expect(additionOf([0], [0])).toEqual([0])
+  expect(additionOf([5], [2])).toEqual([7])
+  expect(additionOf([2], [5])).toEqual([7])
+  expect(additionOf([4], [0])).toEqual([4])
+  expect(additionOf([0], [4])).toEqual([4])
+  expect(additionOf([1, 2], [4])).toEqual([5, 2])
+  expect(additionOf([1, 2], [2, 1])).toEqual([3, 3])
+  expect(additionOf([0, 0, 3], [1, 2, 0, 4])).toEqual([1, 2, 3, 4])
+  expect(additionOf([0, 2, 0, 4], [1, 0, 3])).toEqual([1, 2, 3, 4])
 })()
