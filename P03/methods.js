6b5e3767d3cdd04de6f2b7161ec40de038ba96d3
Grzegorz Chojnacki <grzegorzc60@gmail.com>
2021-01-13 20:28:52 +0100

    Implemented methods

--- P03/methods.js
+++ P03/methods.js
@@ -1,9 +1,33 @@
 'use strict'
 
-const f = (x, y) => -5*x**4 + 2*Math.sqrt(y + x**5 - 5) + 5
-const h = (n, b) => (b - 1) / n
+const a      = 1
+const f      = (x, y) => -5*x**4 + 2*Math.sqrt(y + x**5 - 5) + 5
+const yExact = x => -(x**5) + x**2 + 5
+const step   = (n, b) => (b - a) / n
 
-// Expected return type: { value: number, error: number }
-const euler         = ({n, b}) => { return { value: n, error: b }}
-const eulerModified = ({n, b}) => { return { value: n, error: b }}
-const heun          = ({n, b}) => { return { value: n, error: b }}
+const calculate = (n, h, next, x = a, y = 5, error = 0) => {
+  for (let k = 0; k < n; k++) {
+    y = next(x, y)
+    x = x + h
+    error = Math.max(Math.abs(y - yExact(x)), error)
+  }
+  return { value: y, error: error }
+}
+
+const euler = ({n, b}) => {
+  const h = step(n, b)
+  const fn = (x, y) => y + h * f(x, y)
+  return calculate(n, h, fn)
+}
+
+const eulerModified = ({n, b}) => {
+  const h = step(n, b)
+  const fn = (x, y) => y + h * f(x + h/2, y + h/2 * f(x, y))
+  return calculate(n, h, fn)
+}
+
+const heun = ({n, b}) => {
+  const h = step(n, b)
+  const fn = (x, y) => y + h/2 * (f(x, y) + f(x + h, y + h * f(x, y)))
+  return calculate(n, h, fn)
+}
